a4b8b1e42a073deefb55e84061aab3e6cccc0870
Pavel Zarubin <paha996@yandex.ru>
2023-03-11 01:03:33 +0300

    Fix background keys

--- frontend/src/components/Key.vue
+++ frontend/src/components/Key.vue
@@ -1,13 +1,22 @@
 <script lang="ts" setup>
 import {keyboard} from "../../wailsjs/go/models";
+import { computed } from 'vue'
 
 const props = defineProps<{
   k: keyboard.Key
 }>()
+
+const background = computed<string>(() => {
+  if ((props.k.red < 20) && (props.k.green < 20) && (props.k.blue < 20)) {
+    return 'rgba('+props.k.red+', '+props.k.green+', '+props.k.blue+', 0)'
+  }
+
+  return 'rgba('+props.k.red+', '+props.k.green+', '+props.k.blue+', 1)'
+})
 </script>
 
 <template>
-  <div @click="$emit('click')" :style="{background: 'rgb('+k.red+', '+k.green+', '+k.blue+')'}" class="key" :class="k.name.toLowerCase()">
+  <div @click="$emit('click')" :style="{background: background}" class="key" :class="k.name.toLowerCase()">
     {{ k.name }}
   </div>
 </template>
